774bd72b9946abc5a38c5f2450abdf32791d2ec9
foitelija <foitelija777@gmail.com>
2022-06-30 21:13:14 +0300

    Добавьте файлы проекта.

---
+++ Server/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server // NOTE: IN THIS CASE -> I use .NET 6 with .NET 5 template.
+{
+    class Program
+    {
+        const int port = 8080;
+        static TcpListener listener;
+        static void Main(string[] args)
+        {
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+                listener.Start();
+                Console.WriteLine("Ожидание подключений...");
+
+                while (true)
+                {
+                    Console.WriteLine("Соединения установлено");
+                    TcpClient client = listener.AcceptTcpClient();
+                    ServerLogic serverLogic = new ServerLogic(client);
+
+                    // создаем новый поток для обслуживания нового клиента
+                    Thread clientThread = new Thread(new ThreadStart(serverLogic.Process));
+                    clientThread.Start();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (listener != null)
+                {
+                    listener.Stop();
+                }
+            }
+        }
+    }
+}
---
+++ Server/ServerLogic.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    public class ServerLogic
+    {
+        public int bucket3 = 0;
+        public int bucket5 = 0;
+        public int capacity = 0;
+        public int steps = 0;
+
+
+        private readonly TcpClient _tcpClient;
+
+        public ServerLogic(TcpClient tcpClient)
+        {
+            _tcpClient = tcpClient;
+        }
+
+        public void Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
